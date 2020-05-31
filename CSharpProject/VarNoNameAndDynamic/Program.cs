dee1669b6b77054ad8dbf8531ffb416dae2cd1a9
hainguyen9x4 <hainguyen9x4@gmail.com>
2020-05-31 22:34:23 +0700

    Var and dymanic

---
+++ CSharpProject/VarNoNameAndDynamic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarNoNameAndDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new
+            {
+                abc = 123,
+                str = "Hello"
+            };
+            TestFunc(obj);
+            Console.ReadKey();
+        }
+        static void TestFunc(dynamic dvar)
+        {
+            Console.WriteLine(dvar.abc);
+        }
+    }
+}
