01b345eb49cb7ecf7f586c6819ee650c6e6c6a09
hainguyen9x4 <hainguyen9x4@gmail.com>
2020-05-31 22:17:31 +0700

    Add example for delegate

--- CSharpProject/Delegate/Program.cs
+++ CSharpProject/Delegate/Program.cs
@@ -22,11 +22,37 @@ namespace Delegate
             PrintString0 = Print1;
             string str = "deleagete";
             //PrintString0 += (str);
+            Console.WriteLine("Func---------------------------------------------");
+            Func<int, int> Func1;
+            Func1 = Sum1;
+            Console.WriteLine("Value1 = " + Func1(1));
+            Func<int, int,int> Func2;
+            Func2 = Sum2;
+            Console.WriteLine("Value2 = " + Func2(1,99));
+            Func<int, int, string> Func3;
+            Func3 = Tinhtong;
+            Console.WriteLine("Value2 = " + Func3(1, 99));
+            Console.WriteLine("Action---------------------------------------------");
+            Action<string> Action1;
+            Action1 = Print1;
+            Action1("hello world");
             Console.ReadKey();
         }
         public static  void Print1(string strMessage1)
         {
             Console.WriteLine(string.Format("===>{0}<===", strMessage1));
+        }
+        public static int Sum1(int a)
+        {
+            return a +100;
+        }
+        public static int Sum2(int a,int b)
+        {
+            return a + b;
+        }
+        public static string Tinhtong(int a, int b)
+        {
+            return "Ket qua "+(a + b);
         }
         public static  void Print2(string strMessage2)
         {
