56a2e2484ef58680d0a9b35e9d83a2f8c961b6cb
hainguyen9x4 <hainguyen9x4@gmail.com>
2020-05-31 09:08:58 +0700

    add event with timer
    add event with timer

---
+++ CSharpProject/CSharpProject/ClassBReceiveEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class ClassBReceiveEvent
+    {
+        ClassAFireEvent m_classA;
+        public ClassBReceiveEvent(){
+            m_classA = new ClassAFireEvent();
+            m_classA.DataReceived -= MessageDataReceived;
+            m_classA.DataReceived += MessageDataReceived;
+            m_classA.ErrorReceived -= MessageErrorReceived;
+            m_classA.ErrorReceived += MessageErrorReceived;
+            m_classA.StartTimerFireDataEvent(1000);
+            m_classA.StartTimerFireErrorEvent(2000);
+
+        }
+        private void MessageDataReceived(object sender, DataEventArgs e)
+        {
+            DataEventArgs e_data = new DataEventArgs();
+            e_data.ReceivedData = e.ReceivedData;
+            Console.WriteLine(e_data.ReceivedData);
+        }
+        private void MessageErrorReceived(object sender, ErrorEventArgs e)
+        {
+            ErrorEventArgs e_data = new ErrorEventArgs();
+            e_data.ErrorCode = e.ErrorCode;
+            e_data.ErrorMessage = e.ErrorMessage;
+            Console.WriteLine("Error code:"+e_data.ErrorCode+"Error mes: "+ e.ErrorMessage);
+        }
+    }
+}
---
+++ CSharpProject/CSharpProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class Program
+    {
+        static public void Main()
+        {
+            
+            ClassBReceiveEvent m_classB = new ClassBReceiveEvent();
+            Console.ReadKey();
+        }
+    }
+
+}
